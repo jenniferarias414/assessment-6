5f89a0b118e00c507be77e86376014dfe101ed8c
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-23 09:29:04 -0600

    duelduo tests

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -1,4 +1,4 @@
-const { Builder, Browser, By, until } = require("selenium-webdriver");
+const { Builder, Browser, By, until, Key } = require("selenium-webdriver");
 
 let driver;
 
@@ -10,9 +10,19 @@ afterEach(async () => {
   await driver.quit();
 });
 
+// must have ^^ this ^^ code before adding in automated tests with Selenium
+
 describe("Duel Duo tests", () => {
-  test("page loads with title", async () => {
+  test("page loads with title", async () => { //async fxn to allow 'await' to happen
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+  // Check that clicking the Draw button displays the div with id = “choices”
+  test("draw btn displays div id'choices' ", async () => {
+    await driver.get("http://localhost:8000");
+    await driver.findElement(By.id("draw")).click();
+    await driver.wait(
+      until.elementLocated(By.id('choices')), 1000);
+  });
+
 });
