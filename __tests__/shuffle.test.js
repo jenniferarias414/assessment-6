73e253f1843e8a18f356f47118acb4d14d680bd3
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-23 08:17:06 -0600

    Jest update

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,26 @@
 const shuffle = require("../src/shuffle");
 
+test("checking if Jest works", () => {
+  expect(2).toBe(2)
+}) //Jest works :)
+
 describe("shuffle should...", () => {
   // CODE HERE
+  test('return an array', () => {
+    const result = shuffle([1, 2, 3, 4, 5]);
+    expect(Array.isArray(result)).toBe(true);
+});
+
+test('return an array of the same length as the argument', () => {
+  const inputArray = [1, 2, 3, 4, 5];
+  const result = shuffle(inputArray);
+  expect(result).toHaveLength(inputArray.length);
+});
+
+test('have shuffled items around', () => {
+  const inputArray = [1, 2, 3, 4, 5];
+  const result = shuffle(inputArray);
+  expect(result).not.toBe(inputArray); //toEqual works also...
+});
+
 });
